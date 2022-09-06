19faa072c28087b87eec1ef56924e25cf80c13a8
Leo <Aqu4men@gmail.com>
2022-09-06 16:09:18 +0200

    player movement proto

--- Gamejam rentree/Assets/Leo/Script/playertest.cs
+++ Gamejam rentree/Assets/Leo/Script/playertest.cs
@@ -4,10 +4,42 @@ using UnityEngine;
 
 public class playertest : MonoBehaviour
 {
-
+    [SerializeField] private Transform[] points;
+    Transform destination;
+    [SerializeField] int index = 0;
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            index++;
+
+            if(index == 6)
+            {
+                index = 0;
+            }
+
+
+            Debug.Log(index);
+        }
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            index--;
+
+            if (index == -1)
+            { 
+                index = 5;
+            }
+
+            Debug.Log(index);
+        }
         
+        AtoB(points[index]);
+    }
+    void AtoB(Transform destination)
+    {
+        //transform.position = destination.position;
+        transform.position = Vector3.Lerp(transform.position, destination.position,10*Time.deltaTime);
     }
 }
