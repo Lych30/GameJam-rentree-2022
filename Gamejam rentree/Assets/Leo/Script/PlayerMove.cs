1adddffbd9c3cb8f1f31cc461e5b5abf2f7b86a3
Leo <Aqu4men@gmail.com>
2022-09-07 17:00:36 +0200

    gasbar

--- Gamejam rentree/Assets/Leo/Script/PlayerMove.cs
+++ Gamejam rentree/Assets/Leo/Script/PlayerMove.cs
@@ -8,8 +8,8 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        SpeedMultiplier += Time.deltaTime / 10;
-        SpeedMultiplier = Mathf.Clamp(SpeedMultiplier, 1,3);
+        SpeedMultiplier += Time.deltaTime / 15;
+        SpeedMultiplier = Mathf.Clamp(SpeedMultiplier, 1,5);
        
         transform.position += new Vector3(0, 0, 1) * Time.deltaTime*4 * SpeedMultiplier;
     }
