ed03aa7043f102390bac7c545aa054eeff80be59
Leo <Aqu4men@gmail.com>
2022-09-08 10:42:10 +0200

    Scoring

---
+++ Gamejam rentree/Assets/Leo/Score.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    public TextMeshProUGUI ScoreLeft;
+    public TextMeshProUGUI ScoreRight;
+    public float Multiplier = 1;
+    private float Scoring = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scoring += Time.deltaTime * 100 * Multiplier;
+        Scoring = UnityEngine.Mathf.Round(Scoring);
+        UpdateScore();
+    }
+    void UpdateScore()
+    {
+        ScoreRight.text = Scoring.ToString();
+    }
+}
